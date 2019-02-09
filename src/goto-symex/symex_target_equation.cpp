bd12026a54fe09f4865fe28df6e0ade76d9dcf8d
Peter Schrammel <peter.schrammel@diffblue.com>
2019-02-09 23:16:15 +0000

    Hidden field is used for hiding in trace
    not 'ignore' (which is used for slicing).

--- src/goto-symex/symex_target_equation.cpp
+++ src/goto-symex/symex_target_equation.cpp
@@ -527,7 +527,7 @@ void symex_target_equationt::convert_assertions(
     {
       // ignore already converted assertions in the error trace
       if(step.is_assert() && step.converted)
-        step.ignore = true;
+        step.hidden = true;
 
       if(step.is_assert() && !step.ignore && !step.converted)
       {
@@ -554,7 +554,7 @@ void symex_target_equationt::convert_assertions(
   {
     // ignore already converted assertions in the error trace
     if(step.is_assert() && step.converted)
-      step.ignore = true;
+      step.hidden = true;
 
     if(step.is_assert() && !step.ignore && !step.converted)
     {
