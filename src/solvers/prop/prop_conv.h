50e3186b048fdbf09d329878efffe6efa355f80e
Peter Schrammel <peter.schrammel@diffblue.com>
2019-03-25 12:04:54 +0000

    Remove prop_convt extensions

--- src/solvers/prop/prop_conv.h
+++ src/solvers/prop/prop_conv.h
@@ -43,20 +43,6 @@ public:
   // specialised variant of get
   virtual tvt l_get(literalt a) const=0;
 
-  // incremental solving
-  virtual void set_frozen(literalt a);
-  virtual void set_frozen(const bvt &);
-  virtual void set_assumptions(const bvt &_assumptions);
-  virtual bool has_set_assumptions() const { return false; }
-  virtual void set_all_frozen() {}
-
-  // returns true if an assumption is in the final conflict
-  virtual bool is_in_conflict(literalt l) const;
-  virtual bool has_is_in_conflict() const { return false; }
-
-  // Resource limits:
-  virtual void set_time_limit_seconds(uint32_t) {}
-
   /// Returns the number of incremental solver calls
   virtual std::size_t get_number_of_solver_calls() const = 0;
 };
